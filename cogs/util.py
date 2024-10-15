00f0670bcd9a492084e792cf8652e2299e60a194
toby <tobyleung112@gmail.com>
2024-10-15 17:44:12 +0900

    initialization

---
+++ cogs/util.py
@@ -0,0 +1,74 @@
+import os
+import sys
+import json
+import typing
+import aiohttp
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+class Util(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    async def cog_load(self):
+        print("Cog util loaded!")
+    
+    @commands.hybrid_command(name='ping', description='returns bot ping' , with_app_command=True)
+    async def ping(self, ctx: commands.Context):
+        ping = round(self.bot.latency, 2) * 1000
+        await ctx.send(f'ping: {ping} ms')
+
+    @commands.hybrid_command(name='perms', description='check user permissions in the server', with_app_command=True)
+    async def perms(self, ctx:commands.Context):
+        perm_list = [perm[0] for perm in ctx.author.guild_permissions if perm[1]]
+        await ctx.send(f'{ctx.author.name}\n{perm_list}')
+
+    @commands.hybrid_command(name='restart', description='restarts the bot', with_app_command=True)
+    @commands.is_owner()
+    async def restart(self, ctx: commands.Context):
+        await ctx.send('restarting...')
+        print(f'{ctx.author} ({ctx.author.id}) is restarting the bot...')
+        os.execv(sys.executable, ['python'] + sys.argv)
+
+    @app_commands.command(name='embed', description='outputs an embed from json')
+    @commands.has_permissions(administrator=True)
+    async def embed(self, interaction: discord.Interaction, channel: typing.Union[discord.TextChannel, discord.Thread], *, input: str):
+        e = json.loads(input)
+        embed = discord.Embed.from_dict(e)
+        await channel.send(embed=embed)
+        await interaction.response.send_message(f'embed sent to {channel.mention}!', ephemeral=True)
+
+    @app_commands.command(name='echo', description='make the bot say something')
+    @commands.has_permissions(administrator=True)
+    async def echo(self, interaction: discord.Interaction, channel: typing.Union[discord.TextChannel, discord.Thread], *, input: str):
+        await channel.send(input)
+        await interaction.response.send_message(f'message sent to {channel.mention}!', ephemeral=True)
+
+    @commands.command(name='sync')
+    @commands.is_owner()
+    async def sync(self, ctx: commands.Context):
+        await ctx.send('syncing commands...')
+        sync = await ctx.bot.tree.sync()
+        await ctx.send(f'all {len(sync)} commands synced!')
+
+    @commands.hybrid_command(name='game', description='set what potato is playing', with_app_command=True)
+    @commands.has_permissions(administrator=True)
+    @app_commands.choices(type=[
+        app_commands.Choice(name='Playing', value='Playing'),
+        app_commands.Choice(name='Streaming', value='Streaming'),
+        app_commands.Choice(name='Listening', value='Listening'),
+        app_commands.Choice(name='Watching', value='Watching')
+    ])
+    async def game(self, ctx: commands.Context, type: str, *, activity: str):
+        if type == 'Playing':
+            await self.bot.change_presence(activity=discord.Game(name=activity))
+        elif type == 'Streaming':
+            await self.bot.change_presence(activity=discord.Streaming(name=activity))
+        elif type == 'Listening':
+            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=activity))
+        elif type == 'Watching':
+            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
+        await ctx.send(f'activity set to {type} {activity}')
+async def setup(bot):
+    await bot.add_cog(Util(bot))
---
+++ main.py
@@ -0,0 +1,40 @@
+import discord
+import os
+import logging
+from discord.ext import commands
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'), intents=intents)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+tree = bot.tree
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+async def setup_hook():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+            print(f"Loaded Cog: {filename[:-3]}")
+    await tree.sync()
+
+bot.setup_hook = setup_hook
+
+bot.activity = discord.Game('cars')
+
+@bot.event
+async def on_ready():
+    await print(f'logged in as {bot.user}')
+
+@bot.listen('on_message')
+async def on_message(message: discord.Message):
+    if message.author == bot.user:
+        return
+    if bot.user.mentioned_in(message):
+        await message.channel.send('pui?')
+
+bot.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
